3e4f041c7667677c90d48e9d1bcf9783756163ba
Nguyên <nguyen@gmail.com>
2025-07-14 03:49:54 +0700

    Cập nhật FE - URL request L1

--- FE/src/utils/request.js
+++ FE/src/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://taskflow-jog8.onrender.com/";
 const token = localStorage.getItem("token");
 export const get = async (path) => {
     const response = await fetch(API_DOMAIN + path, {
